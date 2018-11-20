658a748319e4b84542159b6be7665348ca03a501
Suresh <suresh@thinkbridge.in>
2018-11-20 08:57:20 +0530

    Add login and register part

---
+++ Library/Controllers/LibraryController.cs
@@ -0,0 +1,58 @@
+﻿using Library.Models;
+using LibraryServise;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library.Controllers
+{
+    [Authorize]
+    public class LibraryController : Controller
+    {
+        private ApplicationUserManager _userManager;
+        private readonly IUserService userService;
+        private readonly IBookService bookService;
+        public LibraryController(IUserService _userService, IBookService _bookService)
+        {
+            userService = _userService;
+            bookService = _bookService;
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        // GET: Library
+        public async System.Threading.Tasks.Task<ActionResult> HomeAsync()
+        {
+            BooksViewModel model = new BooksViewModel();
+
+            model.books = bookService.getAll().ToList();
+            var userId = User.Identity.GetUserId();
+            //var user = await UserManager.GetRoles();
+            //model.User = userService.getUserRolle(User.Identity.GetUserId());
+            
+            model.User = "Admin";
+            return View(model);
+        }
+        
+        
+        [Authorize(Roles ="")]
+        public ActionResult UploadBook()
+        {
+            return View();
+        }
+    }
+}
---
+++ Library/Models/BooksViewModel.cs
@@ -0,0 +1,14 @@
+﻿using DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Library.Models
+{
+    public class BooksViewModel
+    {
+        public List<Books> books { get; set; }
+        public string User { get; set; }
+    }
+}
